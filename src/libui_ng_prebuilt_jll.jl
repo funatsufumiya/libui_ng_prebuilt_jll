236de29c44b0ad08d5577b08da9e5a505d52542a
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-04 17:13:10 +0900

    feat: get_lib_path()

--- src/libui_ng_prebuilt_jll.jl
+++ src/libui_ng_prebuilt_jll.jl
@@ -6,4 +6,23 @@ function get_artifact_path()
     return artifact"libui_ng_prebuilt_jll"
 end
 
+function get_lib_path()
+    libui_lib = "libui.so"
+
+    @static if Sys.iswindows()
+        libui_lib = "libui.dll"
+    elseif Sys.isapple()
+        libui_lib = "libui.dylib"
+    else
+        libui_lib = "libui.so"
+    end
+
+    p = joinpath(get_artifact_path(), "lib", libui_lib)
+    if isfile(p)
+        return p
+    else
+        return nothing
+    end
+end
+
 end # module libui_ng_prebuilt_jll
